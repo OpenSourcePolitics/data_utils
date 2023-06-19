2fe35599b905024d4c1198c52bcfeebb7d5c73fc
THIVEND <baptiste.thivend@protonmail.com>
2023-06-19 11:49:02 +0200

    Initial commit

--- data_utils/doccano_import/main.py
+++ data_utils/doccano_import/main.py
@@ -1,11 +1,18 @@
-from doccano_client import DoccanoClient
+from doccano_client.beta import DoccanoClient, controllers
 from dotenv import dotenv_values
 
 CONFIG = dotenv_values('data_utils/doccano_import/.env')
 
 def main(): 
+    # import pdb; pdb.set_trace()
     client = DoccanoClient(CONFIG['DOCCANO_URL'])
     client.login(
         username=CONFIG['DOCCANO_USERNAME'],
         password=CONFIG['DOCCANO_PASSWORD']
     )
+
+    print('LIST OF ALL PROJECTS')
+    print('____________________')
+    # import pdb; pdb.set_trace()
+    # print(client.list_projects())
+
